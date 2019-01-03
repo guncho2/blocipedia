748dabb816330328735f7f5052e979200cd9902b
Eduardo Lowensohn <gunchosoft@gmail.com>
2019-01-02 23:48:23 -0500

    story1-5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_30_041720) do
+ActiveRecord::Schema.define(version: 2019_01_03_022620) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
